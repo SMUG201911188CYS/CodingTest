82ffda878cc4da0837ae23801c8841938ed0c02a
Superdodge <yjh01209@gmail.com>
2024-07-17 14:59:48 +0900

    Update 행렬_테두리_회전하기.cpp

--- 윤재형/2021_Dev-Matching_웹_백엔드_개발자(상반기)/Level_2/행렬_테두리_회전하기.cpp
+++ 윤재형/2021_Dev-Matching_웹_백엔드_개발자(상반기)/Level_2/행렬_테두리_회전하기.cpp
@@ -1,1 +1,52 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) 
+{
+    vector<int> answer;
+    int Num = 1;
+    int MAP[100][100];
+    
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++) MAP[i][j] = Num++;
+
+    for(auto q : queries){
+        int x = q[0]; x--;
+        int y = q[1]; y--;
+        int xx = q[2]; xx--;
+        int yy = q[3]; yy--;
+
+        int Min_Num;
+        int Temp = MAP[x][y];
+        Min_Num = Temp;
+        for(int i = x; i < xx; i++)
+        {
+            Min_Num = min(Min_Num, MAP[i][y]);
+            MAP[i][y] = MAP[i + 1][y];
+        }
+        for(int i = y; i < yy; i++)
+        {
+            Min_Num = min(Min_Num, MAP[xx][i]);
+            MAP[xx][i] = MAP[xx][i + 1];
+        }
+        for (int i = xx; i > x; i--)
+        {
+            Min_Num = min(Min_Num, MAP[i][yy]);
+            MAP[i][yy] = MAP[i - 1][yy];
+        }
+        for (int i = yy; i > y; i--)
+        {
+            Min_Num = min(Min_Num, MAP[x][i]);
+            MAP[x][i] = MAP[x][i - 1];
+        }
+        MAP[x][y + 1] = Temp;
+        
+        answer.push_back(Min_Num);
+    }
+    
+    return answer;
+}
 
